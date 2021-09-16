1c1bf9cf227cc6df3086e954ba76dab4776c9a8a
duyhan99 <duyhan1602@gmail.com>
2021-09-16 17:11:46 +0700

    navbar almost done

---
+++ app/js/script.js
@@ -0,0 +1,8 @@
+const btnHamburger = document.querySelector('#btnHamburger')
+btnHamburger.addEventListener('click', function(){
+    if(btnHamburger.classList.contains('open')){
+        btnHamburger.classList.remove('open')
+    }else{
+        btnHamburger.classList.add('open')
+    }
+})
