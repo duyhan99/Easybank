9f6ef3055ed71809558d261263cdec323734e9e8
duyhan99 <duyhan1602@gmail.com>
2021-09-16 21:42:46 +0700

    mobile menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,8 +1,15 @@
 const btnHamburger = document.querySelector('#btnHamburger')
-btnHamburger.addEventListener('click', function(){
-    if(btnHamburger.classList.contains('open')){
-        btnHamburger.classList.remove('open')
-    }else{
-        btnHamburger.classList.add('open')
+const header = document.querySelector('.header')
+const overlay = document.querySelector('.overlay')
+
+btnHamburger.addEventListener('click', function(){ //close
+    if(header.classList.contains('open')){
+        header.classList.remove('open')
+        overlay.classList.remove('fade-in')
+        overlay.classList.add('fade-out')
+    }else{ //open
+        header.classList.add('open')
+        overlay.classList.remove('fade-out')
+        overlay.classList.add('fade-in')
     }
 })
